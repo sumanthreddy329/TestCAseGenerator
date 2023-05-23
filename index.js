4b8eaf689539e71140a5ee19391f319a0228d287
sumanthreddy329 <44049951+sumanthreddy329@users.noreply.github.com>
2023-05-23 14:07:33 +0530

    Update index.js
    New Updated JS

--- index.js
+++ index.js
@@ -1,75 +1,148 @@
+function generateTestCases() {
+  var selectedValue = $('#dropdown').val();
 
-        function generateTestCases() {
-            var methodName = document.getElementById('methodName').value;
-            var classNames = document.getElementById('classNames').value;
-            var classValues = document.getElementById('classValues').value;
-            var controllerName = document.getElementById('controllerName').value;
-            var returnValue = document.getElementById('returnValue').value;
+  if (selectedValue === 'longIdRet') {
+    generateTestCasesForLongIdRet();
+  } else if (selectedValue === 'classObjRet') {
+    generateTestCasesForClassObjRet();
+  }
+}
 
-            // Check if all required fields are filled
-            if (!methodName || !classNames || !classValues || !controllerName || !returnValue) {
-                alert('Please fill in all the required fields.');
-                return;
-            }
+function generateTestCasesForLongIdRet() {
+  var methodName = $('#methodName').val();
+  var classValues = $('#classValues').val();
+  var controllerName = $('#controllerName').val();
+  var returnValue = $('#returnValue').val();
 
-            // Split the class names and generate statements for each class
-            var classNamesArray = classNames.split(',');
-            var classValuesArray = classValues.split(',');
-            var statements = [];
-            for (var i = 0; i < classNamesArray.length; i++) {
-                var className = classNamesArray[i].trim();
-                var classNameSmall = className.toLowerCase();
-                var classValue = classValuesArray[i].trim();
-                var statement = className + ' ' + classNameSmall + ' = learningService.loadEntity(' + className + '.class, "' + classValue + '");';
-                statements.push(statement);
-            }
+  // Check if all required fields are filled
+	if (!methodName) {
+		alert('Please fill in all the required fields.');
+	return;
+  }
 
-            // Generate the test cases
-            var testCase = `
+  // Split the class values and generate statements for each value
+  var classValuesArray = classValues.split(',');
+  var statements = [];
+  for (var i = 0; i < classValuesArray.length; i++) {
+    var classValue = classValuesArray[i].trim();
+    statements.push('Long.parseLong("' + classValue + '")');
+  }
+
+  // Generate the test cases
+  var testCase = `
 @Test
 public void ${methodName}_works() {
-    Person person = super.createTnlAdmin();
-    ${statements.join('\n    ')}
-    super.createPersonExpectations(person);
-    ModelAndView mvRet = new ModelAndView();
-    ${controllerName} controller = createController();
-    when(sessionService.getSessionPerson()).thenReturn(person);
-    final TestHtmlHelper html = new TestHtmlHelper();
-    PdsHelper h = mock(PdsHelper.class);
-    html.setPdsHelper_TESTONLY(h);
-    when(${returnValue}).thenReturn(mvRet);
-    ModelAndView mv = controller.${methodName}(Long.parseLong("${classValuesArray[0]}"));
-    Assert.assertSame(mvRet, mv);
+  Person person = super.createTnlAdmin();
+  super.createPersonExpectations(person);
+  ModelAndView mvRet = new ModelAndView();
+  ${controllerName} controller = createController();
+  when(sessionService.getSessionPerson()).thenReturn(person);
+  final TestHtmlHelper html = new TestHtmlHelper();
+  PdsHelper h = mock(PdsHelper.class);
+  html.setPdsHelper_TESTONLY(h);
+  when(${returnValue}).thenReturn(mvRet);
+  ModelAndView mv = controller.${methodName}(${statements.join(', ')});
+  Assert.assertSame(mvRet, mv);
 }
 
 @Test
 public void ${methodName}_noUser() {
-    super.createNoPersonExpectations();
-    ${controllerName} controller = createController();
-    ModelAndView mv = controller.${methodName}(Long.parseLong("${classValuesArray[0]}"));
-    super.validateModelAndViewIsUnAuthorized(mv);
+  super.createNoPersonExpectations();
+  ${controllerName} controller = createController();
+  ModelAndView mv = controller.${methodName}(${statements.join(', ')});
+  super.validateModelAndViewIsUnAuthorized(mv);
 }`;
 
-            // Set the test cases as the result
-            var resultContainer = document.getElementById('result');
-            resultContainer.innerHTML = '<code>' + testCase + '</code><br><button id="copyButton" onclick="copyCode()">Copy</button>';
+  // Set the test cases as the result
+  var resultContainer = $('#result');
+  resultContainer.html('<code>' + testCase + '</code><br><button id="copyButton" onclick="copyCode()">Copy</button>');
+}
 
-            // Clear the input fields
-            document.getElementById('methodName').value = '';
-            document.getElementById('classNames').value = '';
-            document.getElementById('classValues').value = '';
-            document.getElementById('controllerName').value = '';
-            document.getElementById('returnValue').value = '';
-        }
+function generateTestCasesForClassObjRet() {
+  var methodName = $('#methodName').val();
+  var classNames = $('#classNames').val();
+  var classValues = $('#classValues').val();
+  var controllerName = $('#controllerName').val();
+  var returnValue = $('#returnValue').val();
 
-        function copyCode() {
-            var codeSnippet = document.querySelector('#result code');
-            var range = document.createRange();
-            range.selectNode(codeSnippet);
-            window.getSelection().removeAllRanges();
-            window.getSelection().addRange(range);
-            document.execCommand('copy');
-            window.getSelection().removeAllRanges();
-            alert('Code snippet copied to clipboard!');
-        }
-		
+  // Check if all required fields are filled
+  if (!methodName || !classNames || !classValues || !controllerName || !returnValue) {
+    alert('Please fill in all the required fields.');
+    return;
+  }
+
+  // Split the class names and generate statements for each class
+  var classNamesArray = classNames.split(',');
+  var classValuesArray = classValues.split(',');
+  var statements = [];
+  for (var i = 0; i < classNamesArray.length; i++) {
+    var className = classNamesArray[i].trim();
+    var classNameSmall = className.toLowerCase();
+    var classValue = classValuesArray[i].trim();
+    var statement = className + ' ' + classNameSmall + ' = learningService.loadEntity(' + className + '.class, "' + classValue + '");';
+    statements.push(statement);
+  }
+
+  // Generate the test cases
+  var testCase = `
+@Test
+public void ${methodName}_works() {
+  Person person = super.createTnlAdmin();
+  ${statements.join('\n    ')}
+  super.createPersonExpectations(person);
+  ModelAndView mvRet = new ModelAndView();
+  ${controllerName} controller = createController();
+  when(sessionService.getSessionPerson()).thenReturn(person);
+  final TestHtmlHelper html = new TestHtmlHelper();
+  PdsHelper h = mock(PdsHelper.class);
+  html.setPdsHelper_TESTONLY(h);
+  when(${returnValue}).thenReturn(mvRet);
+  ModelAndView mv = controller.${methodName}(Long.parseLong("${classValuesArray[0]}"));
+  Assert.assertSame(mvRet, mv);
+}
+
+@Test
+public void ${methodName}_noUser() {
+  super.createNoPersonExpectations();
+  ${controllerName} controller = createController();
+  ModelAndView mv = controller.${methodName}(Long.parseLong("${classValuesArray[0]}"));
+  super.validateModelAndViewIsUnAuthorized(mv);
+}`;
+
+  // Set the test cases as the result
+  var resultContainer = $('#result');
+  resultContainer.html('<code>' + testCase + '</code><br><button id="copyButton" onclick="copyCode()">Copy</button>');
+}
+
+function copyCode() {
+  var codeSnippet = $('#result code')[0];
+  var range = document.createRange();
+  range.selectNode(codeSnippet);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  alert('Code snippet copied to clipboard!');
+}
+
+$(document).ready(function () {
+  // Hide the card initially
+  $('.card').hide();
+
+  // Show/hide card and load script based on dropdown selection
+  $('#dropdown').on('change', function () {
+    // Clear the result container
+    $('#result').empty();
+
+    var selectedValue = $(this).val();
+
+    if (selectedValue === 'longIdRet') {
+      $('.card').show();
+      $('.class-names').hide();
+    } else if (selectedValue === 'classObjRet') {
+      $('.card').show();
+      $('.class-names').show();
+    }
+  });
+});
+
